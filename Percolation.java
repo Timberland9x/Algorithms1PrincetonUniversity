0983d83c926619ea93d58033831c024e1e20f307
Timberland9x <choistewart9x@yahoo.com>
2024-04-07 15:27:20 -0700

    added more for Percolation func

--- Percolation.java
+++ Percolation.java
@@ -6,11 +6,13 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 	// creates n-by-n grid, with all sites initially blocked
 	boolean[][] grid;
+	WeightedQuickUnionUF g;
 
 	public Percolation(int n) {
-		boolean[][] grid = new boolean[0][0];
-		for (int row = 0; row < n; row++) {
-			for (int col = 0; col < n; col++) {
+		g = new WeightedQuickUnionUF(n);
+		boolean[][] grid = new boolean[n][n];
+		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= n; col++) {
 				grid[row][col] = false;
 			}
 		}
@@ -19,23 +21,29 @@ public class Percolation {
 	// opens the site (row, col) if it is not open already
 	public void open(int row, int col) {
 		grid[row][col] = true;
+		// 4 if statements to union all 4 sides
+		if (g.connected(row + 1, col) != true) {
+			g.union(row + 1, col);
+		}
 	}
 
 	// is the site (row, col) open?
 	public boolean isOpen(int row, int col) {
-		if(grid[row][col] == true)
+		if (grid[row][col] == true)
 			return true;
-		return true;
+		return false;
 	}
 
 	// is the site (row, col) full?
 	public boolean isFull(int row, int col) {
-		return true;
+		if (grid[row][col] == true)
+			return true;
+		return false;
 	}
 
 	// returns the number of open sites
 	public int numberOfOpenSites() {
-		return 1;
+		return g.count();
 	}
 
 	// does the system percolate?
@@ -46,9 +54,6 @@ public class Percolation {
 
 	// test client (optional)
 	public static void main(String[] args) {
-		Percolation haha = new Percolation(20);
-		System.out.println();
-
 		System.out.println("Test Print");
 	}
 }

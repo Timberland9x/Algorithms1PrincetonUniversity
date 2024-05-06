4d7976c85071ce1de2bf1fe5051765ea9764eb68
Timberland9x <choistewart9x@yahoo.com>
2024-05-06 12:09:33 -0700

    added

--- Percolation.java
+++ Percolation.java
@@ -7,8 +7,13 @@ public class Percolation {
 	// creates n-by-n grid, with all sites initially blocked
 	boolean[][] grid;
 	WeightedQuickUnionUF g;
+	int amt;
 
 	public Percolation(int n) {
+		amt = n;
+		if (n <= 0) {
+			throw new IllegalArgumentException("n <= 0");
+		}
 		g = new WeightedQuickUnionUF(n);
 		boolean[][] grid = new boolean[n][n];
 		for (int row = 1; row <= n; row++) {
@@ -20,6 +25,11 @@ public class Percolation {
 
 	// opens the site (row, col) if it is not open already
 	public void open(int row, int col) {
+
+		if (!withinIndex(row, col)) {
+			throw new IllegalArgumentException("not within row & col");
+		}
+
 		grid[row][col] = true;
 		// 4 if statements to union all 4 sides
 		if (g.connected(row + 1, col) != true) {
@@ -29,6 +39,9 @@ public class Percolation {
 
 	// is the site (row, col) open?
 	public boolean isOpen(int row, int col) {
+		if (!withinIndex(row, col)) {
+			throw new IllegalArgumentException("not within row & col");
+		}
 		if (grid[row][col] == true)
 			return true;
 		return false;
@@ -36,6 +49,9 @@ public class Percolation {
 
 	// is the site (row, col) full?
 	public boolean isFull(int row, int col) {
+		if (!withinIndex(row, col)) {
+			throw new IllegalArgumentException("not within row & col");
+		}
 		if (grid[row][col] == true)
 			return true;
 		return false;
@@ -55,5 +71,13 @@ public class Percolation {
 	// test client (optional)
 	public static void main(String[] args) {
 		System.out.println("Test Print");
+	}
+
+	private boolean withinIndex(int row, int col) {
+		if (row < 0 || row > amt || col < 0 || col > amt)
+			return false;
+		else
+			return true;
+
 	}
 }
